be42700a83ceceb7a7ffde6e1630891bb11699f9
Saajidah <Saajidahmoh@outlook.com>
2025-03-30 14:54:24 +0100

    Initial commit

---
+++ JETHome/JETHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JETHomeApp.swift
+//  JETHome
+//
+//  Created by Mohamed, Saajidah on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct JETHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JETHomeTests/JETHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  JETHomeTests.swift
+//  JETHomeTests
+//
+//  Created by Mohamed, Saajidah on 30/03/2025.
+//
+
+import Testing
+@testable import JETHome
+
+struct JETHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
