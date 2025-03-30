61acb7e97490a24261c318ac07d1d0a9b0e8db77
Saajidah <Saajidahmoh@outlook.com>
2025-03-30 15:11:00 +0100

    API Interaction

---
+++ JETHome/JETView.swift
@@ -0,0 +1,7 @@
+//
+//  Home.swift
+//  JETHome
+//
+//  Created by Mohamed, Saajidah on 30/03/2025.
+//
+
---
+++ JETHome/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  JETHome
+//
+//  Created by Mohamed, Saajidah on 30/03/2025.
+//
+
+import Foundation
