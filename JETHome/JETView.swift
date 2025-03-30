971d50c23eaa4a206405b881bf0e2041d343b6ab
Saajidah <Saajidahmoh@outlook.com>
2025-03-30 15:33:27 +0100

    Data display

--- JETHome/JETView.swift
+++ JETHome/JETView.swift
@@ -17,14 +17,16 @@ struct JETView: View {
         VStack {
             ScrollView {
                 Image(systemName: "globe")
-                VStack {
+                VStack(alignment: .leading) {
                     ForEach(viewController.restaurants.prefix(10), id: \.id){
                         restaurant in
                         VStack(alignment: .leading) {
                             Text(restaurant.name)
                                 .font(.title)
+                            
+                            Text(restaurant.cuisines.map { $0.name }.joined(separator: ", "))
+                            Text("\(formatRating(restaurant.rating.starRating)) (\(restaurant.rating.count))")
                         }
-                        
                         
                     }
                 }
@@ -34,6 +36,15 @@ struct JETView: View {
         }
         .onAppear {
             viewController.fetchRestaurantInfo()
+        }
+    }
+    
+    func formatRating( _ rating: Float) -> String {
+        if rating == 0 {
+            return "0"
+        } else {
+            let ratingValue = String(format: "%.2f", rating)
+            return ratingValue.trimmingCharacters(in: CharacterSet(charactersIn: "0").union(.punctuationCharacters))
         }
     }
 }
--- JETHome/JETViewModel.swift
+++ JETHome/JETViewModel.swift
@@ -8,10 +8,12 @@ import Foundation
 
 class JETViewModel : ObservableObject {
     @Published var restaurants: [Restaurant] = []
+    @Published var postcode: String = "E10 6EQ"
+        //"EC4M7RF"
     
     func fetchRestaurantInfo(){
         let APIInteraction = justEatAPIInteraction()
-        APIInteraction.getRestaurantInfo{ restaurants in
+        APIInteraction.getRestaurantInfo(postcode:postcode){ restaurants in
             DispatchQueue.main.async {
                 self.restaurants = restaurants
             
